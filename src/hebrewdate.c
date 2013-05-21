4f147609e7a388d6d25d67148e75c15f25483203
Ary Tebeka <arytebeka@mac.com>
2013-05-21 14:11:34 +0300

    improved attribution

--- src/hebrewdate.c
+++ src/hebrewdate.c
@@ -1,5 +1,25 @@
 // Extracted from http://libhdate.sourceforge.net
-// Adaptation by Ary Tebeka @arytbk
+// Adaptation for Pebble use by Ary Tebeka contact@arytbk.net
+
+/*  libhdate - Hebrew calendar library: http://libhdate.sourceforge.net
+ *
+ *  Copyright (C) 2011-2012 Boruch Baum  <boruch-baum@users.sourceforge.net>
+ *                2004-2007 Yaacov Zamir <kzamir@walla.co.il>
+ *                1984-2003 Amos Shapir
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "hebrewdate.h"
 
