7161efa2d1d06333713b21d3e4cc13b79a4cb2d2
arytbk <contact@arytbk.net>
2013-07-04 11:37:29 +0300

    - Added Toronto and Baysville - Refactored config.h

--- src/config.h
+++ src/config.h
@@ -10,55 +10,58 @@
 // uncomment the next line to show the watch as black on white
 //#define REVERSED 1
 
+// ******** DAYLIGHT SAVING TIME
+// Currently the Pebble SDK does not tell if Daily Saving Time is active, so must set this flag and recompile (yuk!). Use 0 if not-DST, use -1 if DST.
+#define ISDST (-1)
+
+// Configure your location and timezone
 // NOTE: longitude is positive for East and negative for West
 // Use http://itouchmap.com/latlong.html to find the values for your location
-
 
 // Jerusalem
 //#define LATITUDE    (31.752766)
 //#define LONGITUDE 	(35.119600)
+//#define TIMEZONE (+2)
 
 // RAANANA
 //#define LATITUDE    (32.184781)
 //#define LONGITUDE 	(34.871326)
+//#define TIMEZONE (+2)
 
 // BNEI BRAK
  #define LATITUDE    (32.081261)
  #define LONGITUDE 	(34.826726)
+ #define TIMEZONE (+2)
 
 // Paris
 //#define LATITUDE    (48.856638)
 //#define LONGITUDE   (2.352241)
+//#define TIMEZONE (+1)
 
 // New York, NY
 //#define LATITUDE    (40.714353)
 //#define LONGITUDE 	(-74.005973)
+//#define TIMEZONE (-5)
+
+// Toronto
+//#define LATITUDE (43.6481)
+//#define LONGITUDE (-79.4042)
+//#define TIMEZONE (-5)
+
+// Baysville
+//#define LATITUDE (45.148232)
+//#define LONGITUDE (-79.118831)
+//#define TIMEZONE (-5)
 
 // Palo Alto, CA
 //#define LATITUDE    (37.441883)
 //#define LONGITUDE 	(-122.143019)
+//#define TIMEZONE (-8)
 
 // Los Angeles
 //#define LATITUDE    (34.036658)
 //#define LONGITUDE 	(-118.336637)
-
-// ******** Enter your timezone here, for example, if you are GMT+2 (Israel), enter 2
-
-// Israel
- #define TIMEZONE (+2)
-
-// France
-//#define TIMEZONE (+1)
-
-// USA East
-//#define TIMEZONE (-5)
-
-// USA West
 //#define TIMEZONE (-8)
-
-// ******** DAYLIGHT SAVING TIME
-// Currently the Pebble SDK does not tell if Daily Saving Time is active, so must set this flag and recompile (yuk!). Use 0 if not-DST, use -1 if DST.
-#define ISDST (-1)
 
 //****************** ZMANIM *****************
 // NOT YET IMPLEMENTED
